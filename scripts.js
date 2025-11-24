eb9035e6a4e23c418c8eeef5b81a5a94ebbb2080
CousinsAJC <cousinsdrew@gmail.com>
2025-11-23 20:42:43 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -68,7 +68,7 @@ async function fetchWeather(lat, lon) {
 
 function displayWeather(data) {
     const periods = data.properties.periods;
-    let weatherHTML = '<h2>Weather Forecast</h2><br>' + latitude + ', ' + longitude + '<br>';
+    let weatherHTML = '<h2>Weather Forecast<br>' + latitude + ', ' + longitude + '</h2>';
     periods.forEach(period => {
         weatherHTML += `
             <div class="weather-period">
